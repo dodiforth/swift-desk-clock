4211b595382f07db3957aa6c062ce750a9487d49
Dowon Kim <dowoniscool@yahoo.com>
2024-03-18 23:56:16 +0100

    Project initial commit

---
+++ DeskClock/DeskClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskClockApp.swift
+//  DeskClock
+//
+//  Created by Dowon Kim on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DeskClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
